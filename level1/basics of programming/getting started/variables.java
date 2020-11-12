500937543fdc999a08ba9942d22fc60253a6ac20
Sheetal <pandeysheetal777@gmail.com>
2020-11-12 07:03:48 +0530

    Update variables.java

--- level1/basics of programming/getting started/variables.java
+++ level1/basics of programming/getting started/variables.java
@@ -1,2 +1,16 @@
 // 1. There is no operator for exponentiation. 2 ^ 3 is not 8. This is XOR operator.
 // 2. To do exponentiation use Math.pow function. 
+
+   int a = 10; 
+   int b = 20;
+   a = a + b;
+   b = a - b;
+   a = a - b;
+   
+   System.out.println(a + " " + b);
+
+/*It is 20 10
+  Operation1: a = a + b; //a is 30 now
+  Operation2: b = a - b; //b is 10 now
+  Operation3: a = a - b; //30 - 10 takes place */
+// swapping of values takes place.
