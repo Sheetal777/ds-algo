4d489ab738ad9e4e3256aac8a60b594fa9f31dce
Sheetal <pandeysheetal777@gmail.com>
2020-11-12 06:52:10 +0530

    Update printz.java

--- level1/basics of programming/getting started/printz.java
+++ level1/basics of programming/getting started/printz.java
@@ -1,3 +1,6 @@
+//Printing numbers does not requires double inverted commas.
+//System.out.print("10") and System.out.print(10) will have the same output.
+
 import java.util.*;
 
 public class Main {
