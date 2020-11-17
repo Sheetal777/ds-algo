54824c6f0555a4360034d44ad4d17323d22ea47d
Sheetal <pandeysheetal777@gmail.com>
2020-11-17 23:36:16 +0530

    Create num_inverse.java

---
+++ level1/basics of programming/getting started/num_inverse.java
@@ -0,0 +1,12 @@
+// Indexes and numbers are to be swapped to get inverse of a number.
+// For example, 15342 will have an inverse 42315
+
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+  Scanner scn = new Scanner(System.in);
+  int n = scn.nextInt();
+ }
+}
