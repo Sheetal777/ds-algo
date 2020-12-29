de4c8227b1099a89610adda79b8ca568ad59b1c6
Sheetal <pandeysheetal777@gmail.com>
2020-12-29 21:03:11 +0530

    Update prime_factor.java

--- level1/basics of programming/getting started/prime_factor.java
+++ level1/basics of programming/getting started/prime_factor.java
@@ -6,12 +6,18 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         
+        // only check just before the root of given number
+        
         for(int div = 2; div*div<=n; div++){
             while(n%div == 0){
                 n = n/div;
                 System.out.print(div + " ");
             }
         }
+        
+        // for numbers which arelarge & prime
+        // example 46 = 2*23
+        // print the quotient instead of divisor
         
         if (n!=1){
             System.out.print(n);
